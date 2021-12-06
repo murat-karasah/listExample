15891247a9a0142ad4ffe390151fc9dc0d3869ba
Murat Karasah <mkarasah@gmail.com>
2021-12-06 09:38:01 +0300

    2. app

--- listExample/Program.cs
+++ listExample/Program.cs
@@ -7,18 +7,31 @@ namespace listExample
     {
         static void Main(string[] args)
         {
+            //////1.uygulama
+            ////ArrayList a1 = new ArrayList();
+            ////for (char letter = 'A'; letter <= 'Z'; letter++)
+            ////{
+            ////    a1.Add(letter);
 
-            ArrayList a1 = new ArrayList();
-            for (char letter = 'A'; letter <= 'Z'; letter++)
-            {
-                a1.Add(letter);
-                    
-            }
-            a1.Reverse();
-            foreach (var item in a1)
-            {
-                Console.WriteLine(item);
-            }
+            ////}
+            ////a1.Reverse();
+            ////foreach (var item in a1)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+            ///
+
+            //////2.Uygulama
+            ////ArrayList a2 = new ArrayList();
+            ////a2.Add("asd");
+            ////a2.Add("ssd");
+            ////a2.Add("zsd");
+            ////a2.Add("csd");
+            ////a2.Sort();
+            ////foreach (var item in a2)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
 
 
         }
