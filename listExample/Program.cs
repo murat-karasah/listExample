ddaa5d9d4f8edb7df20cb7ee91e94512d6acecab
Murat Karasah <mkarasah@gmail.com>
2021-12-06 09:34:47 +0300

    Add project files.

---
+++ listExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace listExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList a1 = new ArrayList();
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                a1.Add(letter);
+                    
+            }
+            a1.Reverse();
+            foreach (var item in a1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
